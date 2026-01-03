b6969fe8e9de4e3dcd49adf6368a2ab39b4c1d1d
zhangqy2024 <1585831854@qq.com>
2026-01-03 23:16:31 +0800

    Integration: connect game engine with main loop and world logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
-// #include <memory>
+#include <memory>
 
-// #include "Framework/GameManager.hpp"
-// #include "GameWorld/GameWorld.hpp"
+#include "Framework/GameManager.hpp"
+#include "GameWorld/GameWorld.hpp"
 
-// int main(int argc, char **argv) {
-//     std::shared_ptr<WorldBase> world = std::make_shared<GameWorld>();
+int main(int argc, char **argv)
+{
+    std::shared_ptr<WorldBase> world = std::make_shared<GameWorld>();
 
-//     GameManager::Instance().Play(argc, argv, world);
-// }
+    GameManager::Instance().Play(argc, argv, nullptr);
+}
